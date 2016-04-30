8516e5764bb2545a35354ed6d722bce3ed96fb9c
Al Tobey <atobey@netflix.com>
2016-04-29 17:58:56 -0700

    add first pass at a periodic update manager
    Add a facility for setting up and managing things that need periodic
    updates. The first user comes in the next commit: the pagerduty plugin
    needs to pre-cache oncall policies for the !oncall command. Since a few
    modules will want this sort of function, this makes managing these
    functions a part of Hal that plugins can use without having to implement
    everything locally.

---
+++ hal/periodic.go
@@ -0,0 +1,168 @@
+package hal
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type PeriodicFunc struct {
+	Name     string
+	Interval time.Duration
+	Function func()
+	last     time.Time
+	status   string
+	running  bool
+	tick     <-chan time.Time
+	run      chan time.Time
+	exit     chan struct{}
+	starting sync.WaitGroup
+	stopping sync.WaitGroup
+	init     sync.Once
+	mut      sync.Mutex
+}
+
+var periodicData struct {
+	funcs []*PeriodicFunc
+	mut   sync.Mutex
+}
+
+func init() {
+	periodicData.funcs = make([]*PeriodicFunc, 0)
+}
+
+// GetPeriodicFunc finds a periodic function by name and returns a pointer to it.
+// If the name is not found, nil is returned.
+func GetPeriodicFunc(name string) *PeriodicFunc {
+	periodicData.mut.Lock()
+	defer periodicData.mut.Unlock()
+
+	for _, pf := range periodicData.funcs {
+		if pf.Name == name {
+			return pf
+		}
+	}
+
+	return nil
+}
+
+// initialize internal fields, called automatically using pf.init.Do
+func (pf *PeriodicFunc) initialize() {
+	periodicData.mut.Lock()
+	defer periodicData.mut.Unlock()
+
+	pf.status = "initialized"
+	pf.exit = make(chan struct{})
+	pf.tick = make(<-chan time.Time)
+	pf.run = make(chan time.Time)
+}
+
+// loop is the goroutine's program loop
+func (pf *PeriodicFunc) loop() {
+	pf.mut.Lock()
+	pf.status = "running"
+	pf.running = true
+	pf.mut.Unlock()
+
+	pf.starting.Done()
+
+	// TODO: this should capture/handle panics like router.go does
+	for {
+		select {
+		case <-pf.exit:
+			pf.status = "stopped"
+			return
+		case t := <-pf.tick:
+			log.Printf("PeriodicFunc tick %q @ %s", pf.Name, t)
+			pf.runFunc(t)
+		case t := <-pf.run:
+			log.Printf("PeriodicFunc run %q @ %s", pf.Name, t)
+			pf.runFunc(t)
+		}
+	}
+
+	pf.mut.Lock()
+	pf.running = false
+	pf.mut.Unlock()
+
+	pf.stopping.Done()
+}
+
+// runFunc runs the provided function while holding the pf's mutex.
+func (pf *PeriodicFunc) runFunc(t time.Time) {
+	pf.mut.Lock()
+	defer pf.mut.Unlock()
+
+	pf.last = t
+	pf.Function()
+}
+
+// Register puts a pf in the global list and makes it available to GetPeriodicFunc.
+// Anonymous pf's work fine but are not retreivable.
+func (pf *PeriodicFunc) Register() {
+	found := GetPeriodicFunc(pf.Name)
+	if found != nil {
+		log.Printf("Found duplicate name %q in list of PeriodicFuncs while registering.", pf.Name)
+	}
+
+	periodicData.mut.Lock()
+	defer periodicData.mut.Unlock()
+
+	periodicData.funcs = append(periodicData.funcs, pf)
+}
+
+// Start a periodic function.
+func (pf *PeriodicFunc) Start() {
+	pf.init.Do(pf.initialize)
+
+	pf.mut.Lock()
+
+	pf.tick = time.Tick(pf.Interval)
+	pf.starting.Add(1)
+
+	go pf.loop() // will block on pf.mut until Unlock()
+
+	pf.mut.Unlock()
+
+	pf.starting.Wait() // wait for the goroutine to call Done()
+
+	// run the first pass immediately
+	pf.run <- time.Now()
+}
+
+// Stop a periodic function.
+func (pf *PeriodicFunc) Stop() {
+	pf.init.Do(pf.initialize)
+	pf.mut.Lock()
+	defer pf.mut.Unlock()
+
+	pf.exit <- struct{}{}
+}
+
+// Bump schedules a periodic function to update outside of the scheduled times.
+// The value of pf.Last() is updated when this is used.
+func (pf *PeriodicFunc) Bump() {
+	pf.init.Do(pf.initialize)
+	pf.mut.Lock()
+	defer pf.mut.Unlock()
+
+	pf.run <- time.Now()
+}
+
+// Status returns initialized/running/stopped state as a string.
+func (pf *PeriodicFunc) Status() string {
+	pf.init.Do(pf.initialize)
+	pf.mut.Lock()
+	defer pf.mut.Unlock()
+
+	return pf.status
+}
+
+// Last returns the wallclock time of the last run of the function.
+func (pf *PeriodicFunc) Last() time.Time {
+	pf.init.Do(pf.initialize)
+	pf.mut.Lock()
+	defer pf.mut.Unlock()
+
+	return pf.last
+}
