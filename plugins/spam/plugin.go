11272c222169682bbb5b3e708cdd8ec5bc5b95a1
Al Tobey <atobey@netflix.com>
2016-11-14 10:19:54 -0800

    add a configurable spam plugin

---
+++ plugins/spam/plugin.go
@@ -0,0 +1,43 @@
+package spam
+
+/*
+ * Copyright 2016 Albert P. Tobey <atobey@netflix.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import (
+	"log"
+	"time"
+
+	"github.com/netflix/hal-9001/hal"
+)
+
+var booted time.Time
+
+func Register() {
+	p := hal.Plugin{
+		Name: "spam",
+		Func: spam,
+	}
+	p.Register()
+}
+
+func spam(evt hal.Evt) {
+	response := evt.AsPref().SetUser("").FindKey("spam-response").One()
+	if response.Success {
+		evt.Reply(response.Value)
+	} else {
+		log.Printf("spam is configured in room %q but could not find the 'spam-response' pref", evt.RoomId)
+	}
+}
