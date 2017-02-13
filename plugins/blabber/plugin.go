76e96e0cb53ccfc6e0088e484757cf01fb5db0e0
Al Tobey <atobey@netflix.com>
2017-02-13 10:40:40 -0800

    update & correct copyright notices
    Corrected the copyright assignment on most files.
    
    Updated copyright dates.
    
    Added license to a few files that were missing it too.

--- plugins/blabber/plugin.go
+++ plugins/blabber/plugin.go
@@ -5,7 +5,7 @@
 package blabber
 
 /*
- * Copyright 2016 Albert P. Tobey <atobey@netflix.com>
+ * Copyright 2016-2017 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- plugins/cross_the_streams/plugin.go
+++ plugins/cross_the_streams/plugin.go
@@ -2,7 +2,7 @@
 package cross_the_streams
 
 /*
- * Copyright 2016 Albert P. Tobey <atobey@netflix.com>
+ * Copyright 2016-2017 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
