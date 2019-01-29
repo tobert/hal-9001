b278878b203335fee7db481db43758856b716fa9
Amy Tobey <tobert@github.com>
2019-01-28 23:32:23 -0800

    create a semi-working sshchat broker
    This broker makes it possible to connect hal to an sshchat instance
    using an unencrypted ssh private key file.
    
    It's not complete yet but basically working so I can run the pluginmgr
    and attach uptime and use it.

--- brokers/sshchat/broker.go
+++ brokers/sshchat/broker.go
@@ -17,16 +17,33 @@ package sshchat
  */
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/tobert/hal-9001/hal"
+	"golang.org/x/crypto/ssh"
 )
 
+const ansiCleanReSrc = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+const parseMsgReSrc = `^\[\w+\] (\w+): (.*)\s*$`
+
 var log hal.Logger
+var ansiCleanRe, parseMsgRe *regexp.Regexp
 
 // Broker interacts with the sshchat service.
 type Broker struct {
-	inst string // the instance name of the broker
+	inst       string // the instance name of the broker
+	sshConfig  *ssh.ClientConfig
+	sshClient  *ssh.Client
+	sshSession *ssh.Session
+	stdin      chan string
+	stdout     chan string
+	stderr     chan string
 }
 
 type Config struct {
@@ -36,13 +53,85 @@ type Config struct {
 
 func init() {
 	log.SetPrefix("brokers/sshchat")
+	ansiCleanRe = regexp.MustCompile(ansiCleanReSrc)
+	parseMsgRe = regexp.MustCompile(parseMsgReSrc)
 }
 
 func (c Config) NewBroker(name string) Broker {
-	b := Broker{
-		inst: name,
+	var sshConf ssh.ClientConfig
+
+	sshConf.SetDefaults()
+	sshConf.User = c.SSHUsername
+	sshConf.Auth = c.loadSSHKey()
+	sshConf.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+
+	// TODO: don't hardcode the address
+	client, err := ssh.Dial("tcp", "localhost:2022", &sshConf)
+	if err != nil {
+		log.Fatalf("Unable to connect to sshchat server: %s", err)
+	} else {
+		log.Printf("ohai")
 	}
-	return b
+
+	sess, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Could not create an ssh session: %s", err)
+	}
+	// TODO: session.Close()
+
+	// create buffered channels that will be fed by goroutines
+	// that tranlate to the ssh session's stdio
+	inChan := make(chan string, 10)
+	outChan := make(chan string, 10)
+	errChan := make(chan string, 10)
+
+	// create pipes to connect the ssh session to our goroutines
+	inPipeWr, _ := sess.StdinPipe()
+	outPipeRd, _ := sess.StdoutPipe()
+	errPipeRd, _ := sess.StderrPipe()
+
+	// fire up the forwarders
+	go forwardReaderToChan(outPipeRd, outChan)
+	go forwardReaderToChan(errPipeRd, errChan)
+	go forwardWriterToChan(inPipeWr, inChan)
+
+	return Broker{
+		inst:       name,
+		sshConfig:  &sshConf,
+		sshClient:  client,
+		sshSession: sess,
+		stdin:      inChan,
+		stdout:     outChan,
+		stderr:     errChan,
+	}
+}
+
+// also cleans ansi text to return plain text
+func forwardReaderToChan(reader io.Reader, ch chan string) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		text := scanner.Text()
+		log.Printf("stdin: %q", text)
+		nocr := strings.TrimSuffix(text, "\r")
+		clean := ansiCleanRe.ReplaceAllString(nocr, "")
+		ch <- clean
+	}
+	if scanner.Err() != nil {
+		log.Fatalf("Error while reading from ssh: %s", scanner.Err())
+	}
+}
+
+func forwardWriterToChan(writer io.WriteCloser, ch chan string) {
+	for {
+		select {
+		case text := <-ch:
+			if strings.HasSuffix(text, "\n") {
+				writer.Write([]byte(text))
+			} else {
+				writer.Write([]byte(text + "\r\n"))
+			}
+		}
+	}
 }
 
 // Name returns the name of the broker as set in NewBroker.
@@ -51,7 +140,10 @@ func (b Broker) Name() string {
 }
 
 func (b Broker) Send(evt hal.Evt) {
-	log.Panic("Send() not implemented yet")
+	lines := strings.Split(evt.Body, "\n")
+	for _, line := range lines {
+		b.stdin <- line
+	}
 }
 
 func (b Broker) SendAsSnippet(evt hal.Evt) {
@@ -115,6 +207,42 @@ func (b Broker) HasRoom(room string) bool {
 
 // Stream is an event loop for messages from the ssh channel.
 func (b Broker) Stream(out chan *hal.Evt) {
+	log.Printf("listening on sshchat...")
+
+	for {
+		select {
+		case msg := <-b.stdout:
+			now := time.Now()
+
+			matches := parseMsgRe.FindStringSubmatch(msg)
+			if len(matches) != 3 {
+				log.Printf("Unable to parse message %q", msg)
+				continue
+			}
+
+			user := matches[1]
+			body := matches[2]
+
+			e := hal.Evt{
+				ID:       fmt.Sprintf("%d.%06d", now.Unix(), now.UnixNano()),
+				User:     user,
+				UserId:   user,
+				Room:     "lobby",
+				RoomId:   "lobby",
+				Body:     body,
+				Time:     now,
+				Broker:   b,
+				IsChat:   true,
+				Original: &msg,
+			}
+
+			out <- &e
+		case msg := <-b.stderr:
+			log.Printf("Server stderr: %q", msg)
+		}
+	}
+
+	log.Printf("no longer listening on sshchat...")
 }
 
 func (b Broker) UserIdToName(id string) string {
@@ -151,4 +279,21 @@ func (b Broker) RoomNameToId(name string) string {
 	}
 
 	return name
+}
+
+// requires there to be an unencrypted ssh key in ~/.ssh/insecure-key
+// TODO: replace hard-coded values
+func (c Config) loadSSHKey() []ssh.AuthMethod {
+	kf := c.SSHKeyFile
+	keyText, err := ioutil.ReadFile(kf)
+	if err != nil {
+		log.Fatalf("Could not read ssh private key file %s: %s", kf, err)
+	}
+
+	signer, err := ssh.ParsePrivateKey(keyText)
+	if err != nil {
+		log.Fatalf("Failed to parse ssh private key file %s: %s", kf, err)
+	}
+
+	return []ssh.AuthMethod{ssh.PublicKeys(signer)}
 }
